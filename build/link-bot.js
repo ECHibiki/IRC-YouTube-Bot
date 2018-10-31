1b1da299bea97c1a5b1131614dbea38d768e9ec1
ECHibiki <34425597+ECHibiki@users.noreply.github.com>
2018-10-31 02:32:50 -0400

    SRC not properly updated

--- build/link-bot.js
+++ build/link-bot.js
@@ -7,37 +7,42 @@ class DetailsFetcher {
 }
 class Main {
     static getConnectionProperties() {
+        console.log("\tReading Constants");
         var fs = require('fs');
         this.data_constants = JSON.parse(fs.readFileSync('bot-properties.json', 'utf8'));
     }
     static initBot() {
+        console.log("\tInitializing Bot");
         var irc = require("irc");
         var linkifier_bot = new irc.Client(this.data_constants["SERVER"], this.data_constants["NICK"], {
             channels: this.data_constants["CHANNELS"],
             port: this.data_constants["SERVER-PORT"]
         });
-        console.log(this.data_constants["CHANNELS"]);
+        console.log('\t',this.data_constants["CHANNELS"]);
         linkifier_bot.addListener('message', (sender, channel, text, message_obj) => {
             // console.log(message_obj);
-            switch(text){
-                case "!YTBot -v": 
-                linkifier_bot.say(channel, "IRC-YouTube-Bot V0.1 - @Verniy\nhttps://github.com/ECHibiki/IRC-YouTube-Bot");
-                break;
-                case "!YTBot -h": 
-                linkifier_bot.say(channel, "Enter a YouTube link and this bot will output the details.");
-                break;
-                case "!YTBot": 
-                linkifier_bot.say(channel, "!YTBot -v : Output version info\n!YTBot -h : Output help info");
-                break;
-            }       
+            switch (text) {
+                case "!YTBot -v":
+                    linkifier_bot.say(channel, "IRC-YouTube-Bot " + this.data_constants["VERSION"] + " - @Verniy\nhttps://github.com/ECHibiki/IRC-YouTube-Bot");
+                    break;
+                case "!YTBot -h":
+                    linkifier_bot.say(channel, "Enter a YouTube link in the form of 'www.youtube.com/watch?*' or 'youtu.be/*' and this bot will output the details.");
+                    break;
+                case "!YTBot":
+                    linkifier_bot.say(channel, "!YTBot -v : Output version info\n!YTBot -h : Output help info");
+                    break;
+                default:
+                    if (text.includes("www.youtube.com/watch?") || text.includes("youtu.be/")) {
+                        linkifier_bot.say(channel, "That's certainly a youtube link.");
+                    }
+                    break;
+            }
         });
-
-        console.log("Bot Listening");
     }
     static init() {
-        console.log("Bot Initializing");
         this.getConnectionProperties();
         this.initBot();
+        console.log("\tBot Listening");
     }
 }
 Main.init();
