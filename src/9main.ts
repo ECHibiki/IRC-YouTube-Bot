a570bc121332c085c59b3024b45b5ab2bd3b024c
ECHibiki <ECVerniy@gmail.com>
2019-02-25 17:13:50 -0500

    irc Color text on bot messages

--- src/9main.ts
+++ src/9main.ts
@@ -23,16 +23,18 @@ class Main{
         console.log(this.data_constants["CHANNELS"]);
 
         Main.linkifier_bot.addListener('message', (sender:string, channel:string, text:string, message_obj:any) => {
-			console.log("M " + text);
             switch(text){
                 case "!YTBot -v": 
-                Main.linkifier_bot.say(channel, "IRC-YouTube-Bot " + this.data_constants["VERSION"] +" - @Verniy\nhttps://github.com/ECHibiki/IRC-YouTube-Bot");
+                Main.linkifier_bot.say(channel, Main.irc.colors.wrap("dark_blue", "IRC-YouTube-Bot " + this.data_constants["VERSION"] +" - @Verniy\n"
+                + Main.irc.colors.wrap("dark_blue", "https://github.com/ECHibiki/IRC-YouTube-Bot")));
                 break;
                 case "!YTBot -h": 
-                Main.linkifier_bot.say(channel, "Enter a YouTube link in the form of 'www.youtube.com/watch?*' or 'youtu.be/*' and this bot will output the details.");
+                Main.linkifier_bot.say(channel, Main.irc.colors.wrap("dark_blue", "Enter a YouTube link in the form of 'www.youtube.com/watch?*' or 'youtu.be/*' and this bot will output the details."));
                     break;
                 case "!YTBot": 
-                Main.linkifier_bot.say(channel, "!YTBot -v : Output version info\n!YTBot -h : Output help info");
+                Main.linkifier_bot.say(channel, Main.irc.colors.wrap("dark_blue", "!YTBot -v :") 
+                    + " Output version info\n " + Main.irc.colors.wrap("dark_blue", "!YTBot -h :") 
+                    + " Output help info");
                     break;
                 default:
                     var reg_pag = /\b(www\.youtube\.com\/watch\?[\w?=\-&_]+|youtu\.be\/[\w?=\-&_]+)\b/gu;
@@ -46,18 +48,19 @@ class Main{
 		
 		Main.linkifier_bot.addListener('kick', (channel:string, nick:string, by:string, reason:string, message_obj:any) =>{
             console.log("Bot was kicked from " + channel);
-		}
+		});
     }
 
     static displayYouTubeDetails(details_obj:any, channel:string){
         if(details_obj == undefined || details_obj == "" || details_obj.items.length == 0){
             console.log("Failed");
-            Main.linkifier_bot.say(channel, "!YTBot: link error");
+            Main.linkifier_bot.say(channel, Main.irc.colors.wrap("dark_blue", "!YTBot:") + Main.irc.colors.wrap("dark_red", " link error"));
 			return;
         }
         details_obj.items.forEach((details:any, ind:number)=>{
             console.log(ind,channel,details.snippet.title + " [" + details.snippet.channelTitle + "]");
-            Main.linkifier_bot.say(channel,details.snippet.title + " [" + details.snippet.channelTitle + "]");
+            Main.linkifier_bot.say(channel, Main.irc.colors.wrap("magenta",details.snippet.title)
+                + Main.irc.colors.wrap("dark_green"," [" + details.snippet.channelTitle + "]"));
         });
     }
 
